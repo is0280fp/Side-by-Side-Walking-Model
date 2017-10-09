786eb7083b12e04fcca6a7cf844606eef29e8b11
matsushita <is0280fp@ed.ritsumei.ac.jp>
2017-10-09 16:59:04 +0900

    LRFの歩行データを読み込んで、人間の歩行を実装する

---
+++ agents_ver3.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 22 14:27:45 2017
+
+@author: yume
+"""
+
+import numpy as np
+#  from walking_model import walking_model
+
+
+class Agent(object):
+    def __init__(self,
+                 trajectory_me, trajectory_you, subgoals_p, obstacles_p,
+                 planner, d_t=0.1):
+        self.trajectory_me = trajectory_me[:-1]
+        self.trajectory_you = trajectory_you[:-1]
+#        self.p = trajectory_me[-1]
+        self.s = trajectory_me[-1]
+        self.subgoals_p = subgoals_p
+        self.obstacles_p = obstacles_p
+        self.d_t = d_t
+        self.planner = planner
+
+    def measure(self, s_me, s_you):
+        self.trajectory_me.append(s_me)
+        self.trajectory_you.append(s_you)
+
+    def estimate(self):
+        pass
+
+    def decide_action(self):
+        raise NotImplementedError(
+                "You have to extend Agentclass if you want to use this module")
+
+    def move(self):
+        self.s.p = self.s.p + self.v
+        dif = self.subgoals_p[0] - self.s.p
+        self.s.d = dif / np.linalg.norm(dif)
+
+    def __repr__(self):
+        return repr(self.s)
+
+
+class Agent_human(object):
+    def __init__(self,
+                 trajectory_me, trajectory_you, subgoals_p, d_t=0.03):
+        self.trajectory_me = trajectory_me[:-1]
+        self.trajectory_you = trajectory_you[:-1]
+        self.s = trajectory_me[-1]
+        self.subgoals_p = subgoals_p
+        self.d_t = d_t
+
+    def measure(self, s_me, s_you):
+        self.trajectory_me.append(s_me)
+        self.trajectory_you.append(s_you)
+
+    def estimate(self):
+        pass
+
+    def decide_action(self, count):
+        raise NotImplementedError(
+                "You have to extend Agentclass if you want to use this module")
+
+    def move(self):
+        self.s.p = self.v
+        dif = self.subgoals_p[0] - self.s.p
+        self.s.d = dif / np.linalg.norm(dif)
+
+    def __repr__(self):
+        return repr(self.s)
+
+
+class AgentState(object):
+    def __init__(self, p, d=None):  # Noneでdの引数省略可
+        self.p = np.array(p)
+        self.d = np.array(d)
+
+    def __repr__(self):
+        return "state({}, {})".format(self.p, self.d)
+
+
+# Leader
+# plotする範囲を指定、plot数も指定
+class Robot(Agent):
+
+    def decide_action(self):
+        subgoal_p = self.subgoals_p[0]
+        obstacle_p = self.obstacles_p[0]
+        next_p = self.planner.decide_action(
+                self.trajectory_me, self.trajectory_you,
+                subgoal_p, obstacle_p)
+        current_p = self.s.p
+        self.v = next_p - current_p
+
+
+# follower
+# plotする範囲を指定、plot数も指定
+class Human(Agent_human):
+
+    def decide_action(self, count):
+        #  提案モデルに対する比較手法
+#        self.v = walking_model()
+        ps = ([
+        [ 1.39638343552 , -0.670772264944 ] ,
+        [ 1.32672097618 , -0.613402891541 ] ,
+        [ 1.29423710358 , -0.576229029302 ] ,
+        [ 1.26490391443 , -0.536084754334 ] ,
+        [ 1.22321161195 , -0.47479323992 ] ,
+        [ 1.16483836463 , -0.407274773265 ] ,
+        [ 1.14530817675 , -0.313360000718 ] ,
+        [ 1.11968057765 , -0.236743351869 ] ,
+        [ 1.04601448649 , -0.218638502313 ] ,
+        [ 1.00023513822 , -0.155460433211 ] ,
+        [ 0.952399037927 , -0.0957840967972 ] ,
+        [ 0.876120483525 , -0.0725344816746 ] ,
+        [ 0.789454093751 , 0.0024966573176 ] ,
+        [ 0.701204782518 , 0.0535869111234 ] ,
+        [ 0.60321063024 , 0.0943298391902 ] ,
+        [ 0.501403297386 , 0.126244190932 ] ,
+        [ 0.458674073834 , 0.150082486618 ] ,
+        [ 0.374216612929 , 0.174522796145 ] ,
+        [ 0.270766459447 , 0.197432033057 ] ,
+        [ 0.140487449363 , 0.182466849487 ] ,
+        [ 0.0839504584906 , 0.193064345303 ] ,
+        [ 0.00867053063701 , 0.252826921391 ] ,
+        [ -0.0558847139945 , 0.324567074343 ] ,
+        [ -0.123450011454 , 0.386088749925 ] ,
+        [ -0.213389161593 , 0.483747017494 ] ,
+        [ -0.295008485871 , 0.568286730805 ] ,
+        [ -0.334235606277 , 0.665673791635 ] ,
+        [ -0.360719327173 , 0.738451312721 ] ,
+        [ -0.371115465001 , 0.797026736796 ] ,
+        [ -0.403014375994 , 0.836922394399 ] ,
+        [ -0.434378950527 , 0.915403442066 ] ,
+        [ -0.45977540014 , 1.02586037217 ] ,
+        [ -0.457487655687 , 1.14414566633 ] ,
+        [ -0.442752001844 , 1.2568106133 ] ,
+        [ -0.463553999707 , 1.36427615506 ] ,
+        [ -0.492616608844 , 1.41474701055 ] ,
+        [ -0.527708575348 , 1.51294145345 ] ,
+        [ -0.519195359154 , 1.63566784359 ] ,
+        [ -0.486244990646 , 1.75864932594 ] ,
+        [ -0.478019715864 , 1.87588649695 ] ,
+        [ -0.451797856373 , 1.95211506565 ] ,
+        [ -0.442224659187 , 2.02118731345 ] ,
+        [ -0.425487090152 , 2.09929104389 ] ,
+        [ -0.396311266705 , 2.19805512011 ] ,
+        [ -0.365299981093 , 2.30854524504 ] ,
+        [ -0.312803546216 , 2.41014342222 ] ,
+        [ -0.299361592526 , 2.47706637036 ] ,
+        [ -0.275267391074 , 2.54106418093 ] ,
+        [ -0.267412572606 , 2.61736334628 ] ,
+        [ -0.250503196907 , 2.69662228249 ] ,
+        [ -0.234530700857 , 2.80601753206 ] ,
+        [ -0.199066856343 , 2.92899298522 ]])
+        ps = np.array(ps)
+        self.v = ps[count]
---
+++ main_ver3.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 14 21:56:53 2017
+
+@author: yume
+"""
+
+import copy
+import matplotlib.pyplot as plt
+import numpy as np
+import partner_and_self_anticipation_planner
+from agents_ver3 import Robot
+from agents_ver3 import AgentState
+from agents_ver3 import Human
+
+
+class Logger(object):
+    def __init__(self, length_step, subgoals_p, obstacles_p):
+        self.l_s = []  # 現在位置を格納するリスト
+        self.f_s = []  # 現在位置を格納するリスト
+        self.length_step = length_step
+        self.subgoals_p = np.array(subgoals_p)
+        self.obstacles_p = np.array(obstacles_p)
+
+    def log_leader(self, s):
+        self.l_s.append(copy.copy(s))
+
+    def log_follower(self, s):
+        self.f_s.append(copy.copy(s))
+
+    def display(self):
+        l_p = np.array([s.p for s in self.l_s])
+        f_p = np.array([s.p for s in self.f_s])
+        relative_distance = np.sqrt((l_p - f_p) * 2)
+        plt.plot(*l_p.T, "-o", label="Robot")
+        plt.plot(*f_p.T, "*", label="Human")
+        plt.plot(self.subgoals_p.T[0], self.subgoals_p.T[1], "^",
+                 label="Goal")
+#        plt.plot(self.obstacles_p.T[0], self.obstacles_p.T[1], "^",
+#                 label="obstacle")
+        print("relative_distance", relative_distance[-1])
+        plt.xticks(np.arange(-0, 2, 0.2))
+        plt.yticks(np.arange(-1.0, 1, 0.2))  # 表の軸を0~20に固定
+        plt.grid()
+        plt.legend()
+        plt.gca().set_aspect('equal')
+        plt.show()
+#        print("leader.p", self.l_p[-1])
+#        print("follower.p", self.f_p[-1])
+
+    def savefig(self, filename):
+        plt.savefig(filename)
+        self.display()
+        plt.draw()
+
+    def print(self):
+        print("a_s")
+        print("\n".join(
+                ["{}: {}".format(i, s) for i, s in enumerate(logger.l_s)]))
+#  print("\n".join([str(i) + ": " + str(s) for i, s in enumerate(logger.l_s)]))
+        print()
+        print("b_s")
+        print("\n".join(
+                ["{}: {}".format(i, s) for i, s in enumerate(logger.f_s)]))
+        print()
+        print()
+
+
+def make_trajectory(ps):
+    ps = np.array(ps)
+    trajectory = []
+    prev_p = ps[0]
+    state = AgentState(prev_p)
+    trajectory.append(state)
+    for p in ps[1:]:
+        d = p - prev_p
+        state = AgentState(p, d)
+        trajectory.append(state)
+        prev_p = p
+    return trajectory
+
+
+if __name__ == '__main__':
+    # 表描画
+    goal_x = 11
+    relative_pos = 2
+    l_v_max = 3
+    f_v_max = 2
+    trajectory_a = make_trajectory([
+                [ 1.49423710358 , -0.76229029302 ] ,
+                [ 1.46490391443 , -0.736084754334 ] ,
+                [ 1.42321161195 , -0.77479323992 ] ,
+                [ 1.36483836463 , -0.6274773265 ]])
+    trajectory_b = make_trajectory([
+                [ 1.73578850047 , -0.99751806081 ],
+                [ 1.74111587829 , -0.937682491898 ],
+                [ 1.58094249456 , -0.831414990608 ],
+                [ 1.46679422611 , -0.745349506114 ] ])
+    d_t = 0.1
+    num_grid_x = 7
+    num_grid_y = 7
+    search_range_x = 0.05
+    search_range_y = 0.05
+
+    k_o = 0
+    k_rv = 0.01
+    k_rd = 0.5
+    k_ra = 0.5  # ra = relative_angle
+    k_s = 0.1
+    k_ma = 0.01
+    k_mv = 0.05
+    k_mw = 0.01
+    k_pt = 0  # 新しいfactor
+    subgoals_p = [(0.1, 0.3)]
+    obstacles_p = [(3, 2)]
+    length_step = 40
+    relative_angle_a = 0
+    relative_angle_b = 180 - relative_angle_a
+
+    n = 0
+
+#    aさんはwalking_modelに基づいて，経路計画する
+#   bさんはutilityに基づいて，経路計画する
+    planner_a = \
+        partner_and_self_anticipation_planner.PartnerSelfAnticipationPlanner(
+                "a", num_grid_x, num_grid_y, search_range_x, search_range_y,
+                k_o, k_rv, k_rd, k_ra, k_s, k_ma, k_mv, k_mw, k_pt, d_t,
+                relative_angle_a)
+    human_a = Robot(
+        trajectory_a, trajectory_b, subgoals_p, obstacles_p, planner_a)
+
+    human_b = Human(trajectory_b, trajectory_a, subgoals_p)
+
+    logger = Logger(length_step, subgoals_p, obstacles_p)
+
+    logger.log_leader(human_a.s)
+    logger.log_follower(human_b.s)
+
+#        print("length_step", length_step)
+#        print("n", n)
+    while n < length_step:
+
+        human_a.measure(human_a.s, human_b.s)
+        human_b.measure(human_b.s, human_a.s)
+
+        human_a.decide_action()
+        human_b.decide_action(n)
+
+        human_a.move()
+        human_b.move()
+
+        logger.log_leader(human_a.s)
+        logger.log_follower(human_b.s)
+
+        print("step", n)
+        logger.display()
+        logger.print()
+        print("==================================================================================")
+
+        n += 1  # インクリメント
+#    logger.display()
