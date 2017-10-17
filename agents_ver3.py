e8d5f3508b3e0f6daff40a12c77cf1ca6fe65623
matsushita <is0280fp@ed.ritsumei.ac.jp>
2017-10-17 18:42:48 +0900

    制御を一歩目からした場合とそうでない場合に対応させた

--- agents_ver3.py
+++ agents_ver3.py
@@ -10,21 +10,30 @@ import numpy as np
 
 
 class Agent(object):
-    def __init__(self,
-                 trajectory_me, trajectory_you, subgoals_p, obstacles_p,
-                 planner, d_t=0.1):
-        self.trajectory_me = trajectory_me[:-1]
-        self.trajectory_you = trajectory_you[:-1]
-#        self.p = trajectory_me[-1]
-        self.s = trajectory_me[-1]
-        self.subgoals_p = subgoals_p
-        self.obstacles_p = obstacles_p
+    def __init__(self, subgoals, initial_state,
+                 planner, d_t=0.1, trajectory_me=None, trajectory_you=None):
+        if trajectory_me is not None:
+            self.trajectory_me = trajectory_me[:-1]
+        else:
+            self.trajectory_me = []
+
+        if trajectory_you is not None:
+            self.trajectory_you = trajectory_you[:-1]
+        else:
+            self.trajectory_you = []
+
+        self.s = initial_state
+        self.subgoals = subgoals
         self.d_t = d_t
         self.planner = planner
 
-    def measure(self, s_me, s_you):
+    def get_current_subgoal(self):
+        return self.subgoals[0]
+
+    def measure(self, s_me, s_you, environment):
         self.trajectory_me.append(s_me)
         self.trajectory_you.append(s_you)
+        self.environment = environment
 
     def estimate(self):
         pass
@@ -35,7 +44,7 @@ class Agent(object):
 
     def move(self):
         self.s.p = self.s.p + self.v
-        dif = self.subgoals_p[0] - self.s.p
+        dif = self.get_current_subgoal().p - self.s.p
         self.s.d = dif / np.linalg.norm(dif)
 
     def __repr__(self):
@@ -43,12 +52,19 @@ class Agent(object):
 
 
 class Agent_human(object):
-    def __init__(self,
-                 trajectory_me, trajectory_you, subgoals_p, d_t=0.03):
-        self.trajectory_me = trajectory_me[:-1]
-        self.trajectory_you = trajectory_you[:-1]
-        self.s = trajectory_me[-1]
-        self.subgoals_p = subgoals_p
+    def __init__(
+            self, subgoals, initial_state, d_t=0.03, trajectory_me=None, trajectory_you=None):
+        if trajectory_me is not None:
+            self.trajectory_me = trajectory_me[:-1]
+        else:
+            self.trajectory_me = []
+
+        if trajectory_you is not None:
+            self.trajectory_you = trajectory_you[:-1]
+        else:
+            self.trajectory_you = []
+        self.s = initial_state
+        self.subgoal = subgoals[-1]
         self.d_t = d_t
 
     def measure(self, s_me, s_you):
@@ -64,7 +80,7 @@ class Agent_human(object):
 
     def move(self):
         self.s.p = self.v
-        dif = self.subgoals_p[0] - self.s.p
+        dif = self.subgoal.p - self.s.p
         self.s.d = dif / np.linalg.norm(dif)
 
     def __repr__(self):
@@ -83,13 +99,16 @@ class AgentState(object):
 # Leader
 # plotする範囲を指定、plot数も指定
 class Robot(Agent):
+    def closest_obstacle(self):
+        closest_obstacle = self.environment
+        return closest_obstacle
 
     def decide_action(self):
-        subgoal_p = self.subgoals_p[0]
-        obstacle_p = self.obstacles_p[0]
+        current_subgoal = self.get_current_subgoal()
+        closest_obstacle = self.closest_obstacle()
         next_p = self.planner.decide_action(
                 self.trajectory_me, self.trajectory_you,
-                subgoal_p, obstacle_p)
+                current_subgoal, closest_obstacle)
         current_p = self.s.p
         self.v = next_p - current_p
 
@@ -97,8 +116,15 @@ class Robot(Agent):
 # follower
 # plotする範囲を指定、plot数も指定
 class Human(Agent_human):
+    def __init__(
+            self, subgoals, initial_state, d_t=0.03,
+            trajectory_me=None, trajectory_you=None):
+        super(Human, self).__init__(
+            subgoals, initial_state, d_t=0.03,
+            trajectory_me=None, trajectory_you=None)
+        self.count = -1
 
-    def decide_action(self, count):
+    def decide_action(self):
         #  提案モデルに対する比較手法
 #        self.v = walking_model()
         ps = ([
@@ -155,4 +181,5 @@ class Human(Agent_human):
         [ -0.234530700857 , 2.80601753206 ] ,
         [ -0.199066856343 , 2.92899298522 ]])
         ps = np.array(ps)
-        self.v = ps[count]
+        self.count += 1
+        self.v = ps[self.count]
