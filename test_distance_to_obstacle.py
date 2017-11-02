a58a72ba0d74606e29bb7b733d667ee725a9b464
matsushita <is0280fp@ed.ritsumei.ac.jp>
2017-11-02 15:07:51 +0900

    factorのdistance_to_obstacleの単体テストを作成した。テストケースパス可。

--- test_distance_to_obstacle.py
+++ test_distance_to_obstacle.py
@@ -6,12 +6,12 @@ Created on Tue Aug  8 15:10:34 2017
 
 import numpy as np
 import unittest
-from utility import DistanceToObstacle
+import factors
 
 
 class TestDistanceToObstacle(unittest.TestCase):
     def setUp(self):
-        self.factor = DistanceToObstacle(a=20.0, b=0.40)
+        self.factor = factors.DistanceToObstacle(a=20.0, b=0.40, c=0, d_t=1)
 
     def check_distance_to_obstacle(self, obstacle, p, expected):
         actual = self.factor.distance_to_obstacle(p, obstacle)
@@ -27,15 +27,6 @@ class TestDistanceToObstacle(unittest.TestCase):
         self.check_distance_to_obstacle(np.array([-101, -101]), np.array([-1, -1]), np.sqrt(100**2 + 100**2))
         self.check_distance_to_obstacle(np.array([-1.5, -1.2]), np.array([-0.5, -0.2]), np.sqrt(2))
         self.check_distance_to_obstacle(np.array([-1.5, -1.2]), np.array([1, 0.3]), np.sqrt((-2.5)**2 + (-1.5)**2))
-
-    def check_f_obstacle(self, x, a, b, expected):
-        actual = DistanceToObstacle.f_obstacle(x, a, b)
-        np.testing.assert_allclose(expected, actual)
-
-    def test_f_obstacle(self):
-        pass
-#        self.check_f_obstacle(0.6, 20, 0.4, 1)
-#        self.check_f_obstacle(0.4, 20, 0.4, 0)
 
 
 if __name__ == '__main__':
