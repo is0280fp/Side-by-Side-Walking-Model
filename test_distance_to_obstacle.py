d2686091e8d644e81311da6a7459d8cdee790aab
matsushita <is0280fp@ed.ritsumei.ac.jp>
2017-11-07 13:11:19 +0900

    テストの入力をリストにした

--- test_distance_to_obstacle.py
+++ test_distance_to_obstacle.py
@@ -14,19 +14,20 @@ class TestDistanceToObstacle(unittest.TestCase):
         self.factor = factors.DistanceToObstacle(a=20.0, b=0.40, c=0, d_t=1)
 
     def check_distance_to_obstacle(self, obstacle, p, expected):
+        p = np.array(p)
+        obstacle = np.array(obstacle)
         actual = self.factor.distance_to_obstacle(p, obstacle)
         np.testing.assert_allclose(expected, actual)
 
     def test_distance_to_obstacle(self):
-#        pass
-        self.check_distance_to_obstacle(np.array([1, 1]), np.array([0, 0]), np.sqrt(2))
-        self.check_distance_to_obstacle(np.array([1, 1]), np.array([-1, -1]), np.sqrt(8))
-        self.check_distance_to_obstacle(np.array([0, 0]), np.array([1, 1]), np.sqrt(2))
-        self.check_distance_to_obstacle(np.array([-1, -1]), np.array([1, 1]), np.sqrt(8))
-        self.check_distance_to_obstacle(np.array([-101, -101]), np.array([-100, -100]), np.sqrt(2))
-        self.check_distance_to_obstacle(np.array([-101, -101]), np.array([-1, -1]), np.sqrt(100**2 + 100**2))
-        self.check_distance_to_obstacle(np.array([-1.5, -1.2]), np.array([-0.5, -0.2]), np.sqrt(2))
-        self.check_distance_to_obstacle(np.array([-1.5, -1.2]), np.array([1, 0.3]), np.sqrt((-2.5)**2 + (-1.5)**2))
+        self.check_distance_to_obstacle([1, 1], [0, 0], np.sqrt(2))
+        self.check_distance_to_obstacle([1, 1], [-1, -1], np.sqrt(8))
+        self.check_distance_to_obstacle([0, 0], [1, 1], np.sqrt(2))
+        self.check_distance_to_obstacle([-1, -1], [1, 1], np.sqrt(8))
+        self.check_distance_to_obstacle([-101, -101], [-100, -100], np.sqrt(2))
+        self.check_distance_to_obstacle([-101, -101], [-1, -1], np.sqrt(100**2 + 100**2))
+        self.check_distance_to_obstacle([-1.5, -1.2], [-0.5, -0.2], np.sqrt(2))
+        self.check_distance_to_obstacle([-1.5, -1.2], [1, 0.3], np.sqrt((-2.5)**2 + (-1.5)**2))
 
 
 if __name__ == '__main__':
