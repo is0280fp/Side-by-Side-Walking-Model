e445f0d5d4a8e9ba309be1bc337a9e1ae3d2918b
matsushita <is0280fp@ed.ritsumei.ac.jp>
2018-01-30 18:27:31 +0900

    全factorの組み合わせをパラメタ候補に入れるようにメソッドを改修した

--- calibrate_parameters.py
+++ calibrate_parameters.py
@@ -11,9 +11,23 @@ from collections import defaultdict
 
 
 def choose_parameters(start, end, interval):
-    a = np.arange(start, end, interval)
-    b = np.arange(start, end, interval)
-    a_b_set = list(itertools.product(a, b))
+    #    aは分母で０は当てはまらないため
+    if start == 0:
+        start_a = start + 0.01
+    a1 = np.arange(start_a, end, interval)
+    b1 = np.arange(start, end, interval)
+    a2 = np.arange(start_a, end, interval)
+    b2 = np.arange(start, end, interval)
+    a3 = np.arange(start_a, end, interval)
+    b3 = np.arange(start, end, interval)
+    a4 = np.arange(start_a, end, interval)
+    b4 = np.arange(start, end, interval)
+    a5 = np.arange(start_a, end, interval)
+    b5 = np.arange(start, end, interval)
+    a6 = np.arange(start_a, end, interval)
+    b6 = np.arange(start, end, interval)
+    a_b_set = list(itertools.product(
+            a1, b1, a2, b2, a3, b3, a4, b4, a5, b5, a6, b6))
     return a_b_set
 
 
@@ -25,7 +39,7 @@ def calculate_error(ideal_walking_p, model_walking_p):
 if __name__ == '__main__':
     ideal_walking_p = np.random.rand(1000, 1000)
     model_walking_p = np.random.rand(1000, 1000)
-    a_b_set = choose_parameters(0, 5, 0.01)
+    a_b_set = choose_parameters(0, 2, 1)
     error_lst = []
 
     error_paras_match = defaultdict()
