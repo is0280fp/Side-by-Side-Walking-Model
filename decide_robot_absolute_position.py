b1c3cf9e5ff6ad15450c50e09d13571394ae6f25
matsushita <is0280fp@ed.ritsumei.ac.jp>
2018-02-01 00:42:12 +0900

    二点を通る直線の与える二点がp、ｄとなっていて間違っていた。prev_p、ｐが正しいので修正した

--- decide_robot_absolute_position.py
+++ decide_robot_absolute_position.py
@@ -9,20 +9,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#  二点を通る直線の方程式
 def linear_equation(x1, y1, x2, y2):
     a = (y1 - y2) / (x1 - x2)
     b = y1 - a * x1
     return (a, b)
 
 
-def decide_robot_absolute_position(p, d, social_distance):
-    a, b = linear_equation(p[0], p[1], d[0], d[1])
+#  直線上のある点からある距離の先にある点
+def decide_robot_absolute_position(prev_p, p, social_distance):
+    a, b = linear_equation(p[0], p[1], prev_p[0], prev_p[1])
     x1 = p[0]
 #    y1 = p[1]
-    x2 = x1 + social_distance / np.sqrt(a ** 2 + 1)
-    y2 = a * (x1 + social_distance / np.sqrt(a ** 2 + 1)) + b
-    return (x2, y2)
+#   plus
+    x = x1 + social_distance / np.sqrt(a ** 2 + 1)
+    y = a * (x1 + social_distance / np.sqrt(a ** 2 + 1)) + b
+#   minus
+    x2 = x1 - social_distance / np.sqrt(a ** 2 + 1)
+    y2 = a * (x1 - social_distance / np.sqrt(a ** 2 + 1)) + b
+
+    return ((x2, y2), (x, y))
 
 
 if __name__ == '__main__':
-    print(decide_robot_absolute_position((3, 3), (2, 2), np.sqrt(2)))
+    print(decide_robot_absolute_position((1, 1), (3, 3), np.sqrt(2)))
+    print(decide_robot_absolute_position((0, 0), (1, np.sqrt(3)), 2))
